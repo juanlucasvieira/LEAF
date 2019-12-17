c0394eccb90e723b258e5f75c59c5c93f73f46df
Juan Lucas Vieira <juanlucasvieira@id.uff.br>
2019-12-17 15:44:56 -0300

    Add time-stamps to the log messages

--- vAP-controller/src/main/java/br/uff/vAPcontroller/Log.java
+++ vAP-controller/src/main/java/br/uff/vAPcontroller/Log.java
@@ -5,6 +5,11 @@
  */
 package br.uff.vAPcontroller;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  *
  * @author juan
@@ -14,14 +19,19 @@ public class Log {
     public static final int INFO = 1;
     public static final int DEBUG = 2;
     public static final int ERROR = 3;
+    
+    public static DateFormat formatter = new SimpleDateFormat("HH:mm:ss.SSS");
+    
 
     public static void print(int messageType, String msg) {
+        formatter.setTimeZone(TimeZone.getDefault());
+        String dateFormatted = formatter.format(new Date(System.currentTimeMillis()));
         if (messageType == DEBUG && Csts.DEBUG_LOG_LEVEL) {
-            System.out.println(" > " + msg);
+            System.out.println(dateFormatted + " > " + msg);
         } else if (messageType == ERROR) {
-            System.out.println("!!ERROR!! : " + msg);
+            System.out.println(dateFormatted + "!!ERROR!! : " + msg);
         } else if (messageType == Log.INFO){
-            System.out.println(" > " + msg);
+            System.out.println(dateFormatted + " > " + msg);
         }
     }
 
