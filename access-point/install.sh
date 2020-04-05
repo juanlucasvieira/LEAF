041f15d72eadff3139a8809088f4213fdaf6ac53
ASUS-ROG\Juan Lucas Vieira <juanlucasvieira@id.uff.br>
2020-04-05 20:34:41 -0300

    Adds bridge-utils dependency in install.sh

--- access-point/install.sh
+++ access-point/install.sh
@@ -1,2 +1,2 @@
-sudo apt-get -y install libnl-3-dev libnl-genl-3-dev libssl-dev make net-tools
+sudo apt-get -y install libnl-3-dev libnl-genl-3-dev libssl-dev make net-tools bridge-utils
 (cd hostapd/ && make)
